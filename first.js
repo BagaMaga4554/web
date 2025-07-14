c71d6de056db72f8533ae87312bde38ff42cb10d
BagaMaga228 <117483889+BagaMaga4554@users.noreply.github.com>
2025-07-14 12:08:15 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,4 @@
+'use strict';
+const completedTasks = 2;
+const pendingTasks = 3;
+console.log(`Всего задач ${completedTasks + pendingTasks}, завершённых ${completedTasks}, незаконченных ${pendingTasks}`);
